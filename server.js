9a5ed43db4e342edd53a3e9dd5dca4ac706023b9
sriarya <chaithracsy@gmail.comgit config --global user.email chaithracsy@gmail.com>
2022-09-07 18:34:27 +0530

    updating package.json

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
 require('dotenv').config()
 
-app.use(express.urlencoded({ extended: true }))
+const routes = require('./routes');
+
+app.use(express.json())
 const { PORT } = process.env;
+
+// app.use('/', (req, res) => {
+//     console.log("heyy");
+//     res.send("helloo")
+// })
+app.use('/api', routes);
 
 app.listen(PORT, err => {
     if (err) throw err;
