dd561fb34ffff057ca1e9fb3883d774835c06899
sriarya <chaithracsy@gmail.comgit config --global user.email chaithracsy@gmail.com>
2022-09-07 18:35:29 +0530

    adding controllers

---
+++ controllers/auth.js
@@ -0,0 +1,42 @@
+const { userRegistrationService, userLoginService } = require('../services/auth')
+
+const handleUserRegistration = (req, res) => {
+    try {
+        const { body } = req;
+        console.log('reached successfully!!');
+        const response = userRegistrationService(body);
+        if (response.status == 'success') {
+            res.status(200).json(response)
+        } else {
+            res.status(400).json(response)
+        }
+    } catch (err) {
+        res.status(400).json({
+            status: 'failure',
+            message: 'An error occured during registration.Please try after some time.'
+        })
+    }
+}
+
+const handleUserLogin = (req, res) => {
+    try {
+        const { body } = req;
+        console.log('reached successfully!!');
+        const response = userLoginService(body);
+        if (response.status == 'success') {
+            res.status(200).json(response)
+        } else {
+            res.status(400).json(response)
+        }
+    } catch (err) {
+        res.status(400).json({
+            status: 'failure',
+            message: 'An error occured during Login.Please try after some time.'
+        })
+    }
+}
+
+module.exports = {
+    handleUserLogin,
+    handleUserRegistration
+}
