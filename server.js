4a65bfa75a67642cc77943231dd8fc939798de92
sriarya <chaithracsy@gmail.comgit config --global user.email chaithracsy@gmail.com>
2022-09-08 23:12:17 +0530

    adding mongodb connection

--- server.js
+++ server.js
@@ -7,15 +7,21 @@ require('dotenv').config()
 const routes = require('./routes');
 
 app.use(express.json())
-const { PORT } = process.env;
+const { PORT, MONGODB_URL } = process.env;
 
-// app.use('/', (req, res) => {
-//     console.log("heyy");
-//     res.send("helloo")
-// })
-app.use('/api', routes);
+mongoose.connect(MONGODB_URL, { useNewUrlParser: true });
+const db = mongoose.connection;
 
-app.listen(PORT, err => {
-    if (err) throw err;
-    console.log('Server listen on port ', PORT);
+db.on('error', err => {
+    console.log('Mongoose error', err);
+});
+
+db.once('open', async () => {
+    console.log('Connected To', MONGODB_URL);
+    app.use('/api', routes);
+    app.listen(PORT, err => {
+        if (err) throw err;
+        console.log('Server listen on port ', PORT);
+    });
+
 });
